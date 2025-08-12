788a437711c4415f908fdb8c73b3bc31b2a40e91
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-12 02:42:00 -0400

    Configure TypeScript tooling

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
+  env: {
+    browser: true,
+    node: true,
+    es2020: true,
+  },
+};
